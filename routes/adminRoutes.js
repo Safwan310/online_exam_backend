8a62e3f0e423bcd72c41a06853c4b0047c32ef0d
Mohammed Safwan S <mdsafwan3102001@gmail.com>
2021-12-20 18:46:44 +0530

    Added fetching endpoints for admin

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,12 +1,15 @@
 import express from "express";
-import { loginUser, registerUser } from "../controllers/userController.js";
+import { getSubjects, getTests, loginUser, registerUser } from "../controllers/userController.js";
 import { createTest,createSubject } from "../controllers/adminController.js";
+import { protectRoute } from "../middleware/authMiddleware.js";
 
 const adminRoute = express.Router();
 
 adminRoute.post("/register", registerUser);
 adminRoute.get("/login", loginUser);
-adminRoute.post("/createTest",createTest);
-adminRoute.post("/createSubject",createSubject);
+adminRoute.get("/getSubjects",protectRoute,getSubjects);
+adminRoute.get("/getTests",protectRoute, getTests);
+adminRoute.post("/createTest",protectRoute,createTest);
+adminRoute.post("/createSubject",protectRoute,createSubject);
 
 export default adminRoute;
